bbfe4b9f513252de7998affe8bdc445e59d02228
Yogesh-Mourya <128880978+Yogesh-Mourya@users.noreply.github.com>
2026-01-28 22:45:59 +0530

    Add A. Sublime Sequence.cpp for sequence sum calculation

---
+++ 800-1000/A. Sublime Sequence.cpp
@@ -0,0 +1,62 @@
+// Farmer John has an integer x
+// . He creates a sequence of length n
+//  by alternating integers x
+//  and −x
+// , starting with x
+// .
+
+// For example, if n=5
+// , the sequence looks like: x,−x,x,−x,x
+// .
+
+// He asks you to find the sum of all integers in the sequence.
+
+// Input
+// The first line contains an integer t
+//  (1≤t≤100
+// )  — the number of test cases.
+
+// The only line of input for each test case is two integers x
+//  and n
+//  (1≤x,n≤10
+// ).
+
+// Output
+// For each test case, output the sum of all integers in the sequence.
+
+// Example
+// InputCopy
+// 4
+// 1 4
+// 2 5
+// 3 6
+// 4 7
+// OutputCopy
+// 0
+// 2
+// 0
+// 4
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+    
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    
+    
+    while (t--) {
+        int x , n;
+        cin >> x >> n;
+        
+        if(n%2) cout << x << "\n";
+        else cout << 0 << "\n";
+    }
+
+    return 0;
+}
